e47fdbcfd5df809e05c0dc12f644e64a050f079d
tomaszpiernik <s22733.pjwstk.edu.pl>
2022-08-11 12:27:21 +0200

    Initial Project Add

---
+++ src/test/java/pl/tomasz/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomasz.springpetclinic;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
