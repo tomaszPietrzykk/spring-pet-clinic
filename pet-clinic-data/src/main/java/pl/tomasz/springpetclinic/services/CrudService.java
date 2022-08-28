ac0b6aa9f144da67524fe5864baae931803b67b7
tomaszpietrzykk <pietrzyktomek@gmail.com>
2022-08-28 18:09:28 +0200

    refactoring to common crud interface. Closes #12

---
+++ pet-clinic-data/src/main/java/pl/tomasz/springpetclinic/services/CrudService.java
@@ -0,0 +1,17 @@
+package pl.tomasz.springpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findByID(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+
+}
--- pet-clinic-data/src/main/java/pl/tomasz/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/pl/tomasz/springpetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package pl.tomasz.springpetclinic.services;
 
 import pl.tomasz.springpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 }
