7e3c3351bea7c6e7db8099d2ca8073ab508d2d5b
tomaszpietrzykk <pietrzyktomek@gmail.com>
2022-08-29 20:39:56 +0200

    adding vets page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/pl/tomasz/springpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package pl.tomasz.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+
+}
