9082741ef0b31d99c5c851261647c818acdf67cf
tomaszpietrzykk <pietrzyktomek@gmail.com>
2022-08-29 19:41:57 +0200

    Impl Map based Services. Closes #7

---
+++ pet-clinic-data/src/main/java/pl/tomasz/springpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package pl.tomasz.springpetclinic.services.map;
+
+import pl.tomasz.springpetclinic.model.Pet;
+import pl.tomasz.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/pl/tomasz/springpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package pl.tomasz.springpetclinic.services.map;
+
+import pl.tomasz.springpetclinic.model.Vet;
+import pl.tomasz.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
